664be0a2a8e7036212562db28da4788a64f55744
Yara Tavares <yaracriistavares@gmail.com>
2022-04-29 20:14:27 -0300

    fix: add environment REACT_APP_API_URL

--- src/common/services/index.js
+++ src/common/services/index.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000";
-// "https://backend-repositorioprovas.herokuapp.com";
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 function config(token) {
   return { headers: { Authorization: `Bearer ${token}` } };
